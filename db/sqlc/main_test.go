4e2f597b778b6e32ec7df594dca5d758558d1ac6
Raphael Dozzi <rdozzi84@gmail.com>
2026-08-07 09:52:38 -0700

    Implement app.env and config.go file; implemented env vars in account and main_test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,11 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/rdozzi/simple_bank/db/util"
 )
 
 var testQueries *Queries
@@ -19,9 +15,13 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M){
 
-	var err error
+	config, err := util.LoadConfig("../..")
 
-	testDB, err = sql.Open(dbDriver,dbSource)
+	if err != nil{
+		log.Fatal("cannot load config:",err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver,config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
---
+++ db/util/config.go
@@ -0,0 +1,31 @@
+package util
+
+// Config stores all configuration ofhe application
+// The values are read by viper from a config file or environmental variable
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configuration from file or environmental variables
+func LoadConfig(path string) (config Config, err error){
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
