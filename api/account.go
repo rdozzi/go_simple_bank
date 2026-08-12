1a5b8b002dd38a76cb7e381ccf4b67ce0e17b5ae
Raphael Dozzi <rdozzi84@gmail.com>
2026-08-11 20:39:58 -0700

    add custom response for foreign_key_violation and unique_violation for account creation

--- api/account.go
+++ api/account.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	db "github.com/rdozzi/simple_bank/db/sqlc"
 )
 
@@ -29,7 +31,15 @@ func (server *Server) createAccount(ctx *gin.Context){
 
 	account, err := server.store.CreateAccount(ctx,arg)
 	if err != nil {
-		ctx.JSON(http.StatusInsufficientStorage, errorResponse((err)))
+		if pqErr, ok := err.(*pq.Error); ok {
+			log.Println(pqErr.Code.Name())
+			switch pqErr.Code.Name(){
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse((err)))
 		return
 	}
 
