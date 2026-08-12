29e76ff9366a44163b4fcd86cc1889aa4d0c4938
Raphael Dozzi <rdozzi84@gmail.com>
2026-08-11 19:54:40 -0700

    Integrate RandomEmail into random.go file

--- db/util/random.go
+++ db/util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 )
@@ -37,3 +38,9 @@ func RandomCurrency() string{
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
+
+//RandomEmail generates a random email
+func RandomEmail() string{
+	return fmt.Sprintf("%s@email.com",RandomString(6))
+}
+	
