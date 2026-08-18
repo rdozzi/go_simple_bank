d9a1f0e17c68630cce392cdc81f147f2b1cf8ea9
Raphael Dozzi <rdozzi84@gmail.com>
2026-08-17 18:27:35 -0700

    Updated Server struct in server.go to include util.config and token.Maker reference; implemented in main and config

--- db/util/config.go
+++ db/util/config.go
@@ -4,6 +4,8 @@ package util
 // The values are read by viper from a config file or environmental variable
 
 import (
+	"time"
+
 	"github.com/spf13/viper"
 )
 
@@ -11,6 +13,8 @@ type Config struct {
 	DBDriver string `mapstructure:"DB_DRIVER"`
 	DBSource string `mapstructure:"DB_SOURCE"`
 	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey string `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 // LoadConfig reads configuration from file or environmental variables
