d0d11463d34e0b6b3fe0f4aa6a93576c7869c7dc
Raphael Dozzi <rdozzi84@gmail.com>
2026-09-22 18:05:22 -0700

    Add runGatewayServer function to the main.go file and update make proto to include -grpc gateway output command with proto and grpc api

--- main.go
+++ main.go
@@ -2,10 +2,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/rdozzi/simple_bank/api"
 	db "github.com/rdozzi/simple_bank/db/sqlc"
@@ -28,6 +31,7 @@ func main(){
 
 	store := db.NewStore(conn)
 	// runGinServer(config,store)
+	go runGatewayServer(config,store)
 	runGrpcServer(config,store)
 	
 }
@@ -65,5 +69,34 @@ func runGinServer(config util.Config, store db.Store) {
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store){
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create serfer:", err)
+	}
+
+	grpcMux := runtime.NewServeMux()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server:",err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/",grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+
+	log.Printf("start HTTP server at %s", listener.Addr().String())
+	err = http.Serve(listener,mux)
+	if err != nil {
+		log.Fatal("cannot start http server:",err)
 	}
 }
